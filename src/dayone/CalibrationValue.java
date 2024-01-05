9f3631a70f81465b2e81ccad9d1220bc6db0e60a
Chris <chris.kohlmann@googlemail.com>
2024-01-05 19:10:58 +0100

    Create DayOne class

---
+++ src/dayone/CalibrationValue.java
@@ -0,0 +1,16 @@
+
+
+public class CalibrationValue{
+    // Input as List of strings
+    // arrayValue strToInt ('firstIntDigit' + 'lastIntDigit')
+    // sumOf Array[int] = Calibration sum als int
+
+    private Array<String> inputValues = null;
+    
+    public CalibrationValue(Array<String> inputValues){
+        this.inputValues = inputValues;
+    }
+
+    
+
+}
