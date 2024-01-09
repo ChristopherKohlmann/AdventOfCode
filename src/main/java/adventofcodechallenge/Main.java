42bbcd4594bbac170b1042ca5f06fb52032f4189
Chris <chris.kohlmann@googlemail.com>
2024-01-09 17:25:17 +0100

    Finish task two from day one

--- src/main/java/adventofcodechallenge/Main.java
+++ src/main/java/adventofcodechallenge/Main.java
@@ -5,9 +5,11 @@ import java.nio.file.Paths;
 import dayone.CalibrationValue;
 
 public class Main {
-    public static void main(String[] args) {
-         
+
+       public static void main(String[] args) {
+        
         Path path = Paths.get("src/main/java/dayone/input.txt");
+        //Path path = Paths.get("src/main/java/dayone/testInput.txt");
 
         if (!path.toFile().exists()) {
             System.err.println("File not found!");
@@ -19,7 +21,9 @@ public class Main {
         CalibrationValue cv = new CalibrationValue(
             ir.readInput());
 
-        cv.valueSum();
-
+        cv.valueSum(); 
     }
+
+
+
 }
--- src/main/java/dayone/CalibrationValue.java
+++ src/main/java/dayone/CalibrationValue.java
@@ -2,67 +2,104 @@ package dayone;
 
 import java.util.ArrayList;
 
-public class CalibrationValue{
+public class CalibrationValue {
     // Input as List of strings (done)
     // arrayValue strToInt ('firstIntDigit' + 'lastIntDigit')
     // sumOf Array[int] = Calibration sum als int
 
     private ArrayList<String> inputValues = null;
-    
-    public CalibrationValue(ArrayList<String> inputValues){
+
+    private final String[] digitString = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    public CalibrationValue(ArrayList<String> inputValues) {
         this.inputValues = inputValues;
+    }
+
+    private String getFirstDigitInLine(String lineToChange) {
+        String firstDigit = "";
+        int smallestIndex = lineToChange.length();
+        for (String digit : digitString) {
+            if(!lineToChange.contains(digit)){
+                continue;
+            }
+            int indexOfDigitInLine = lineToChange.indexOf(digit);
+            if (indexOfDigitInLine < smallestIndex) {
+                smallestIndex = indexOfDigitInLine;
+                firstDigit = digit;
+            }
+        }
+        return firstDigit;
     }
 
     /**
      * prüfe ob die zeichenketten 'zero','one', 'two',...'nine' enthalten sind
-     * wenn eine enthalten ist replace all found with correct digit
-     * rufe funktion mit neuem string nochmal auf
+     * prüfe welche enthaltene digit als erstes im value vorkommt
+     * mit dieser digit dann replace first found auf den value ausführen
+     * rufe methode mit neuem string nochmal auf
      * wenn keine gefunden wurden return the final string
-     * @param line
+     * 
+     * @param lineToChange
      * @return
      */
-    private String replaceStringWithDigit(String writtenDigit, String line){
-        String newLine = "";
-        String[] digits = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        int selectedDigit = findIndex(digits, writtenDigit);
-        if(line.contains(writtenDigit)){
-            newLine = line.replaceAll(writtenDigit, Integer.toString(selectedDigit));
+    private String replaceDigitStringsWithDigit(String lineToChange) {
+        String newLine = lineToChange;
+        String firstDigitInLine = getFirstDigitInLine(lineToChange);
+        if(firstDigitInLine == ""){
+            return newLine;
         }
-        return newLine;
+        String substitution = Integer.toString(getArabicSubstitutionDigit(firstDigitInLine));
+        int insertIndex = newLine.indexOf(firstDigitInLine)+1;
+        newLine = newLine.substring(0, insertIndex) + substitution + newLine.substring(insertIndex);
+        return replaceDigitStringsWithDigit(newLine);
     }
 
-    private int getDigitFromString(String line){
+    private int getArabicSubstitutionDigit(String digitToConvert) {
+        int n=1;
+        for (String digit : digitString){
+            if (digit == digitToConvert){
+                break;
+            }
+            n++;
+        }
+        return n;
+    }
+
+    private int getDigitFromString(String line) {
         String digits = "";
-        
-        for (char c : line.toCharArray()){
-            if (Character.isDigit(c)){
+
+        for (char c : line.toCharArray()) {
+            if (Character.isDigit(c)) {
                 digits += c;
             }
         }
 
-        if(digits.length() < 2){
+        if (digits.length() < 2) {
             digits += digits;
         }
-        if(digits.length()>2){
+        if (digits.length() > 2) {
             char firstDigit = digits.toCharArray()[0];
-            char lastDigit = digits.toCharArray()[digits.length()-1];
+            char lastDigit = digits.toCharArray()[digits.length() - 1];
             digits = Character.toString(firstDigit) + Character.toString(lastDigit);
         }
         return Integer.parseInt(digits);
     }
 
-    private ArrayList<Integer> digitSelector(ArrayList<String> input){
+    private ArrayList<Integer> digitSelector(ArrayList<String> input) {
         ArrayList<Integer> digits = new ArrayList<Integer>();
 
-        if (inputValues == null){
-            System.err.println("SHit is empty!");
+        if (inputValues == null) {
+            System.err.println("Shit is empty!");
             return null;
         }
 
-        for (String line : inputValues){
-            digits.add(getDigitFromString(line));
+        for (String line : inputValues) {
+            System.err.println(line);
+            // Task Part Two
+            System.err.println(replaceDigitStringsWithDigit(line)); 
+            int digit = getDigitFromString(replaceDigitStringsWithDigit(line));
+            System.err.println(digit);
+            digits.add(digit);
         }
-
         return digits;
     }
 
@@ -70,7 +107,5 @@ public class CalibrationValue{
         int result = digitSelector(this.inputValues).stream().mapToInt(Integer::intValue).sum();
         System.out.println("Fuck the result is: " + result);
     }
-
-    
 
 }
