80242c98ee28eeb10e5393e089b27857a4dacda0
Chris <chris.kohlmann@googlemail.com>
2024-01-15 19:07:30 +0100

    Raw version of inputReader day two
    Co-authored-by: Marcel Witoschek <Marciland@users.noreply.github.com>

--- src/main/java/adventofcodechallenge/Main.java
+++ src/main/java/adventofcodechallenge/Main.java
@@ -6,24 +6,41 @@ import dayone.CalibrationValue;
 
 public class Main {
 
-       public static void main(String[] args) {
-        
-        Path path = Paths.get("src/main/java/dayone/input.txt");
-        //Path path = Paths.get("src/main/java/dayone/testInput.txt");
+    public static void main(String[] args) {
 
+        
+
+        //solveDayOne();
+
+        solveDayTwo();
+    }
+
+    private static void solveDayTwo() {
+        Path path = Paths.get("src/main/java/daytwo/testinputdatwo.txt");
         if (!path.toFile().exists()) {
             System.err.println("File not found!");
             System.exit(1);
         }
         String input = path.toAbsolutePath().toString();
 
-        InputReader ir = new InputReader(input);
-        CalibrationValue cv = new CalibrationValue(
-            ir.readInput());
-
-        cv.valueSum(); 
+        daytwo.InputReader ir = new daytwo.InputReader(input);
+        ir.readInput();
     }
 
+    private static void solveDayOne() {
+        Path path = Paths.get("src/main/java/dayone/input.txt");
+        // Path path = Paths.get("src/main/java/dayone/testInput.txt");
+        if (!path.toFile().exists()) {
+            System.err.println("File not found!");
+            System.exit(1);
+        }
+        String input = path.toAbsolutePath().toString();
 
+        dayone.InputReader ir = new dayone.InputReader(input);
+        CalibrationValue cv = new CalibrationValue(
+                ir.readInput());
+
+        cv.valueSum();
+    }
 
 }
--- src/main/java/daytwo/InputReader.java
+++ src/main/java/daytwo/InputReader.java
@@ -1,10 +1,12 @@
-package adventofcodechallenge;
+package daytwo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InputReader {
 
@@ -14,14 +16,16 @@ public class InputReader {
         this.inputPath = inputPath;
     }
 
-    public ArrayList<String> readInput() {
-        ArrayList<String> result = new ArrayList<String>();
+    public HashMap<Integer, List<Map<String, Integer>>> readInput() {
+        HashMap<Integer, List<Map<String, Integer>>> result = new HashMap<Integer, List<Map<String, Integer>>>();
+        // Map<GameID, Game>
         try (BufferedReader br = new BufferedReader(new FileReader(inputPath))) {
 
             List<String> inputLines = br.lines().toList();
-
+            int n =1;
             for (String eachStr : inputLines) {
-                result.add(eachStr.strip());
+                result.put(n, formatLineString(eachStr));
+                n++;
             }
 
         } catch (IOException e) {
@@ -43,14 +47,31 @@ public class InputReader {
     json: {
         "1": [
             { Red: 1, Green: 2, Blue: 3},
+           origInput -> Game 1: 1 green, 4 blue; 1 blue, 2 green, 1 red; 1 red, 1 green, 2 blue; 1 green, 1 red; 1 green; 1 green, 1 blue, 1 red
             { Red: 1, Green: 2, Blue: 3}
         ],
-        "2": [
-            { Red: 1, Green: 2, Blue: 3},
+  Game-> "2": [ <- List
+            { Red: 1, Green: 2, Blue: 3}, <- HashMap
             { Red: 1, Green: 2, Blue: 3}
         ]
     }
 
     */
+    }
+
+    private List<Map<String, Integer>> formatLineString(String eachStr) {
+        String stringOfSets = eachStr.split(":")[1];
+        String[] arrayOfSets = stringOfSets.split(";");
+        List<Map<String, Integer>> game = new ArrayList<Map<String, Integer>>();
+        for (String set : arrayOfSets) {
+            Map<String, Integer> setMap = new HashMap<String, Integer>();
+            String[] draws = set.split(",");
+            for (String draw : draws){
+                String[] keyValuePairs = draw.strip().split(" ");
+                setMap.put(keyValuePairs[1], Integer.parseInt(keyValuePairs[0]) );
+            }
+            game.add(setMap);
+        }
+        return game;
     };
 }
